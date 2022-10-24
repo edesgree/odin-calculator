c1038307ea0ce7c89b72524607a4b2dc42a07369
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-24 21:52:17 +0200

    debug calc function

--- src/script.js
+++ src/script.js
@@ -26,58 +26,128 @@ const app = () => {
     // const btnDecimal = document.getElementById('decimal');
     // const btnPower = document.getElementById('power');
     const btnEqual = document.getElementById('equal');
-    
-    const output = document.querySelector(".output");
+
+    const display = document.querySelector(".output");
     const outputTop = document.querySelector(".history");
 
-    const allButtons = document.querySelectorAll('[data-type=number],[data-type=operator]');
+    const allButtons = document.querySelectorAll('#calculator button');
     const numberButtons = document.querySelectorAll('[data-type=number]');
     const operatorButtons = document.querySelectorAll('[data-type=operator]');
     console.log(numberButtons);
-    
+
     // for each buttons add to input
     allButtons.forEach(button => button.addEventListener('click', (e) => {
-        const input = e.target.innerText;
-        outputTop.innerText += input;
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+
+        console.log(button);
+        console.log(key);
+        // save the keytype in a dataset on #calculator
+        calculator.dataset.previousKeyType = button.dataset.type;
+        if (key.dataset.type === "number") {
+            console.log("click a nuumber");
+            console.log("previousKeyType" + previousKeyType);
+
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                console.log("displayedNum is 0");
+                display.textContent = keyContent;
+            } else {
+
+                display.textContent = displayedNum + keyContent;
+
+            }
+
+        }
+        if (action) {
+            console.log("click action: " + action);
+        }
+
+        if (action === 'decimal') {
+            console.log("decimal");
+            console.log("displayedNum" + displayedNum);
+            console.log("previousKeyType" + previousKeyType);
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.';
+                console.log("oula")
+            }
+            if (previousKeyType === 'operator') {
+                console.log("hey");
+                display.textContent = '0.';
+            }
+
+        }
+        if (action === 'add' || action === 'substract' || action === 'multiply' || action === 'divide') {
+            // add active state on operator btn
+
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            // Note: It's sufficient to check for firstValue and operator because secondValue always exists
+            if (firstValue && operator && previousKeyType!== 'operator') {
+                const calcValue = operate(operator, firstValue, secondValue);
+                
+                display.textContent = calcValue;
+            }
+            // add custom attribute to calculator
+            key.classList.add('active');
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+        }
+        // remove active state on operator btn
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('active'))
+
+        //calculate
+        if (action === 'calculate') {
+            console.log('calculate!');
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+            display.textContent = operate(operator, firstValue, secondValue);
+
+        }
     }));
     //when operator button call operateButtons
-    operatorButtons.forEach(button => button.addEventListener('click', operatesButton));
+    //operatorButtons.forEach(button => button.addEventListener('click', operatesButton));
 
-    btnEqual.addEventListener('click', calc);
+    // btnEqual.addEventListener('click', calc);
     btnClear.addEventListener('click', clear);
     btnCorrect.addEventListener('click', correct);
 
-    function operatesButton(e) {
-        const input = e.target.innerText;
-        console.log("operatesButton dataset:" + input);
-        console.log("input" + e.target.innerText);
-        if (
-            outputTop.innerText.includes('+') ||
-            outputTop.innerText.includes('*') ||
-            outputTop.innerText.includes('-') ||
-            outputTop.innerText.includes('/')
-        ) {
-            console.log('go calc()');
-            calc();
-        }
-        outputTop.innerText += input;
-console.log("outputTop.innerText"+outputTop.innerText);
-    }
-    // get value , split en fonction du operator return operate(+,1,2)
-    // operate (+,1,2)
+    // function operatesButton(e) {
+    //     const input = e.target.innerText;
+    //     console.log("operatesButton dataset:" + input);
+    //     console.log("input" + e.target.innerText);
+    //     if (
+    //         outputTop.innerText.includes('+') ||
+    //         outputTop.innerText.includes('*') ||
+    //         outputTop.innerText.includes('-') ||
+    //         outputTop.innerText.includes('/')
+    //     ) {
+    //         console.log('go calc()');
+    //         calc();
+    //     }
+    //     outputTop.innerText += input;
+    //     console.log("outputTop.innerText" + outputTop.innerText);
+    // }
 
-    function calc() {
-        //arrondi le result
-        let store = getValue(outputTop.innerText);
-        if (store == null) {
-            return null;
-        } else {
-            outputTop.innerText = Math.floor(store * 10000) / 10000;
-            output.innerText = Math.floor(store * 10000) / 10000;
+    
 
-        }
+    // function calc() {
+    //     //arrondi le result
+    //     let store = getValue(outputTop.innerText);
+    //     if (store == null) {
+    //         return null;
+    //     } else {
+    //         outputTop.innerText = Math.floor(store * 10000) / 10000;
+    //         display.innerText = Math.floor(store * 10000) / 10000;
 
-    }
+    //     }
+
+    // }
 
     function operate(operator, number1, number2) {
         let result;
@@ -89,47 +159,46 @@ console.log("outputTop.innerText"+outputTop.innerText);
                 return null;
             }
             result = num1 / num2;
-        }
-        if (operator === "multiply") {
+        } else if (operator === "multiply") {
             result = num1 * num2;
-        }
-        if (operator === "add") {
+        } else if (operator === "add") {
             result = num1 + num2;
-        }
-        if (operator === "substract") {
+        } else if (operator === "substract") {
             result = num1 - num2;
         }
+        //result =Math.floor(result * 10000) / 10000
         return result;
     }
-    function getValue(formula) {
-        console.log("formula:" + formula);
-        if (formula.includes('+')) {
-            const splitvalues = formula.split('+');
 
-            return operate('add', splitvalues[0], splitvalues[1]);
+    // function getValue(formula) {
+    //     console.log("formula:" + formula);
+    //     if (formula.includes('+')) {
+    //         const splitvalues = formula.split('+');
+    //         console.log("splitvalues[0], splitvalues[1]:" + splitvalues[0] + "," + splitvalues[1]);
+    //         return operate('add', splitvalues[0], splitvalues[1]);
 
-        }
-        if (formula.includes('*')) {
-            const splitvalues = formula.split('*');
+    //     }
+    //     if (formula.includes('*')) {
+    //         const splitvalues = formula.split('*');
+    //         console.log("splitvalues[0], splitvalues[1]:" + splitvalues[0] + "," + splitvalues[1]);
+    //         return operate('multiply', splitvalues[0], splitvalues[1]);
+    //     }
+    //     if (formula.includes('-')) {
+    //         const splitvalues = formula.split('-');
+    //         console.log("splitvalues[0], splitvalues[1]:" + splitvalues[0] + "," + splitvalues[1]);
+    //         return operate('substract', splitvalues[0], splitvalues[1]);
+    //     }
+    //     if (formula.includes('/')) {
+    //         const splitvalues = formula.split('/');
+    //         console.log("splitvalues[0], splitvalues[1]:" + splitvalues[0] + "," + splitvalues[1]);
+    //         return operate('divide', splitvalues[0], splitvalues[1]);
+    //     }
 
-            return operate('multiply', splitvalues[0], splitvalues[1]);
-        }
-        if (formula.includes('-')) {
-            const splitvalues = formula.split('-');
-
-            return operate('substract', splitvalues[0], splitvalues[1]);
-        }
-        if (formula.includes('/')) {
-            const splitvalues = formula.split('/');
-
-            return operate('divide', splitvalues[0], splitvalues[1]);
-        }
-
-    }
+    // }
 
     function clear() {
         outputTop.innerText = "";
-        output.innerText = "";
+        display.innerText = "0";
         console.log("clear");
     }
     function correct() {
@@ -146,60 +215,7 @@ console.log("outputTop.innerText"+outputTop.innerText);
     console.log(operate('*', 2, 3));
     console.log(operate('-', 2, 3));
 
-    /*
-        let arrayCalc1 = [];
-        let arrayCalc2 = [];
-        function display() {
-    
-        }
-    
-        let buttons = Array.from(allButtons);
-        buttons.forEach(button => button.addEventListener('click', (e) => {
-            console.log("buttonid: " + e.target.id);
-            console.log("buttontype: " + e.target.getAttribute("data-type"));
-            // if button clicked is digit, we append it to the number to process
-            if (e.target.getAttribute("data-type") == 'number') {
-                console.log('number!');
-                console.log(displayElement.innerText);
-                displayElement.innerText += e.target.innerText;
-                if(arrayCalc1.length>0){
-                    arrayCalc2.push(e.target.innerText);
-                }else{
-                    
-                arrayCalc1.push(e.target.innerText);
-                }
-    
-    
-            }
-            if ((e.target.getAttribute("data-type") !== 'number')) {
-                arrayCalc1 = arrayCalc1.join("");
-                console.log("join");
-            }
-            if (e.target.getAttribute("data-type") == 'operator') {
-                console.log('operator!');
-                if(arrayCalc1.length>0){
-                    console.log("array1 not empty")
-                }else{
-                    console.log("array1 empty")
-                }
-            }
-    
-            if (e.target.getAttribute("data-type") == 'equal') {
-                console.log('equal!');
-                //operate();
-            }
-            if (e.target.getAttribute("data-type") == 'clear') {
-                console.log('clear!');
-                displayElement.innerText="";
-                
-                //empty the array
-                arrayCalc1 = [];
-                arrayCalc2.length = 0;
-            }
-            console.log("arrayCalc1:" + arrayCalc1);
-            console.log("arrayCalc2:" + arrayCalc2);
-        }))
-        */
+
 }
 
 //play app
