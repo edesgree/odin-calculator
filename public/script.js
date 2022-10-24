ea6e262801a65b90df05552bb9e0ffe7d359b98c
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-24 12:11:48 +0200

    creation of ui variable in js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,31 @@
-// app
+// app Calculator
 const app = () => {
+    // init variables/
+    const result = 0;
 
+    // UI elememts
+    const display = document.getElementById('display');
+    const btnNumber0 = document.getElementById('number-0');
+    const btnNumber1 = document.getElementById('number-1');
+    const btnNumber2 = document.getElementById('number-2');
+    const btnNumber3 = document.getElementById('number-3');
+    const btnNumber4 = document.getElementById('number-4');
+    const btnNumber5 = document.getElementById('number-5');
+    const btnNumber6 = document.getElementById('number-6');
+    const btnNumber7 = document.getElementById('number-7');
+    const btnNumber8 = document.getElementById('number-8');
+    const btnNumber9 = document.getElementById('number-9');
+    const btnPlus = document.getElementById('plus');
+    const btnMinus = document.getElementById('minus');
+    const btnMultiply = document.getElementById('multiply');
+    const btnDivide = document.getElementById('divide');
+    const btnPercentage = document.getElementById('percentage');
+    const btnPlusMinus = document.getElementById('plus-minus');
+    const btnCorrect = document.getElementById('correct');
+    const btnClear = document.getElementById('clear');
+    const btnDecimal = document.getElementById('decimal');
+    const btnPower = document.getElementById('power');
+    const btnEqual= document.getElementById('equal');
 }
 
 //play app
