5364130e15a6960c9f18c9825c99340870f7252b
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-24 14:35:27 +0200

    add function to get user input when clicking button

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const app = () => {
     const result = 0;
 
     // UI elememts
-    const display = document.getElementById('display');
+    const displayElement = document.getElementById('display');
     const btnNumber0 = document.getElementById('number-0');
     const btnNumber1 = document.getElementById('number-1');
     const btnNumber2 = document.getElementById('number-2');
@@ -25,8 +25,70 @@ const app = () => {
     const btnClear = document.getElementById('clear');
     const btnDecimal = document.getElementById('decimal');
     const btnPower = document.getElementById('power');
-    const btnEqual= document.getElementById('equal');
+    const btnEqual = document.getElementById('equal');
+
+    const allButtons = document.querySelectorAll('button');
+    const numberButtons = document.querySelectorAll('[data-type=number]');
+    const opeartorButtons = document.querySelectorAll('[data-type=operator]');
+console.log(numberButtons);
+
+    let arrayCalc1 = [];
+    let arrayCalc2 = [];
+    function display() {
+
+    }
+
+    let buttons = Array.from(allButtons);
+    buttons.forEach(button => button.addEventListener('click', (e) => {
+        console.log("buttonid: " + e.target.id);
+        console.log("buttontype: " + e.target.getAttribute("data-type"));
+        // if button clicked is digit, we append it to the number to process
+        if (e.target.getAttribute("data-type") == 'number') {
+            console.log('number!');
+            console.log(displayElement.textContent);
+            displayElement.textContent += e.target.textContent;
+            if(arrayCalc1.length>0){
+                arrayCalc2.push(e.target.textContent);
+            }else{
+                
+            arrayCalc1.push(e.target.textContent);
+            }
+
+
+        }
+        if ((e.target.getAttribute("data-type") !== 'number')) {
+            arrayCalc1 = arrayCalc1.join("");
+            console.log("join");
+        }
+        if (e.target.getAttribute("data-type") == 'operator') {
+            console.log('operator!');
+            if(arrayCalc1.length>0){
+                console.log("array1 not empty")
+            }else{
+                console.log("array1 empty")
+            }
+        }
+
+        if (e.target.getAttribute("data-type") == 'equal') {
+            console.log('equal!');
+            //operate();
+        }
+        if (e.target.getAttribute("data-type") == 'clear') {
+            console.log('clear!');
+            displayElement.textContent="";
+            
+            //empty the array
+            arrayCalc1 = [];
+            arrayCalc2.length = 0;
+        }
+        console.log("arrayCalc1:" + arrayCalc1);
+        console.log("arrayCalc2:" + arrayCalc2);
+    }))
 }
 
 //play app
 app();
+
+
+// get value , split en fonction du operator return operate(+,1,2)
+// operate (+,1,2)
